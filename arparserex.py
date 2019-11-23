4a472b50c2ba4387df49a02b530d4e44c0f97b92
sebastian.madrazo@gmail.com <sebastian.madrazo@gmail.com>
2019-11-23 20:11:11 -0300

    1

--- arparserex.py
+++ arparserex.py
@@ -21,31 +21,18 @@ def filesearch():    ##Defines module filesearch()
     group.add_argument('--filepath', help='Filename',nargs='?',type=argparse.FileType('r'), required=True)
     group2.add_argument('--version', action='version', version='%(prog)s 1.0')
 
-    args = parser.parse_args()
+    args = parser.parse_args()  #Variable that contains all arguments
     #print vars(args)  ##Prints all arguments passed
     #print (args.string)
 
-    with args.filepath as fp :
+    with args.filepath as fp :  #From the variable args, use filepath as file input for search
             lines = args.filepath.readlines()
             for l in lines:
-                if args.string in l:
+                if args.string in l:    #Variable string from args will be the string to search
                     print(l)
 
 if __name__ == '__main__':  #Define main function
     filesearch()
 
 
-#>>> import argparse
-#>>> 
-#>>> parser = argparse.ArgumentParser(description='Search within a File')
-#>>> parser.add_argument('--string', help='String to search', required=True)    #Default type=str
-#_StoreAction(option_strings=['--string'], dest='string', nargs=None, const=None, default=None, type=None, choices=None, help='String to search', metavar=None)
-#>>> parser.add_argument('--filepath', help='Filename',nargs='?',type=argparse.FileType('r'), required=True) 
-#_StoreAction(option_strings=['--filepath'], dest='filepath', nargs='?', const=None, default=None, type=FileType('r'), choices=None, help='Filename', metavar=None)
-#>>> parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-#_VersionAction(option_strings=['--version'], dest='version', nargs=0, const=None, default='==SUPPRESS==', type=None, choices=None, help="show program's version number and exit", metavar=None)
-#>>> 
-#>>> 
-#>>> 
-#>>> parser.parse_args(['--string','hola','--filepath','/var/log/system.log'])
-#Namespace(filepath=<open file '/var/log/system.log', mode 'r' at 0x10e27f660>, string='hola')
+
